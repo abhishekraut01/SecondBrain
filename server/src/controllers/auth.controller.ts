ebd0766529bb8c9141c8a69afdbe34d3404b16fb
Abhishek <Abhishekraut8697@gmail.com>
2025-02-07 02:16:58 +0530

    implemented email based password reset

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -3,14 +3,17 @@ import User from '../models/user.model';
 import crypto from 'crypto';
 import asyncHandler from '../utils/AsyncHandler';
 import bcrypt from 'bcryptjs';
-import { resetPasswordSchema } from '../validation/validationSchema';
+import {
+  requestResetPasswordSchema,
+  ResetPasswordSchema,
+} from '../validation/validationSchema';
 import ApiError from '../utils/ApiError';
 import sendEmail from '../utils/sendEmail';
 import ApiResponse from '../utils/ApiResponse';
 
 export const requestPasswordReset = asyncHandler(
   async (req: Request, res: Response) => {
-    const validationResult = resetPasswordSchema.safeParse(req.body);
+    const validationResult = requestResetPasswordSchema.safeParse(req.body);
 
     if (!validationResult.success) {
       throw new ApiError(
@@ -39,18 +42,63 @@ export const requestPasswordReset = asyncHandler(
     const message = `You requested a password reset. Click the link below to reset your password:\n\n${resetUrl}\n\nThis link is valid for 10 minutes.`;
 
     try {
-        await sendEmail({
-          to: existingUser.email,
-          subject: 'Password Reset Request',
-          text: message,
-        });
-  
-        res.status(200).json(new ApiResponse(200, 'Password reset email sent'));
-      } catch (err) {
-        existingUser.resetPasswordToken = null;
-        existingUser.resetPasswordExpires = null;
-        await existingUser.save({ validateBeforeSave: false });
-        throw new ApiError(500, 'Email could not be sent');
-      }
+      await sendEmail({
+        to: existingUser.email,
+        subject: 'Password Reset Request',
+        text: message,
+      });
+
+      res.status(200).json(new ApiResponse(200, 'Password reset email sent'));
+    } catch (err) {
+      existingUser.resetPasswordToken = null;
+      existingUser.resetPasswordExpires = null;
+      await existingUser.save({ validateBeforeSave: false });
+      throw new ApiError(500, 'Email could not be sent');
+    }
+  }
+);
+
+export const resetPassword = asyncHandler(
+  async (req: Request, res: Response) => {
+    const validationResult = ResetPasswordSchema.safeParse(req.body);
+
+    if (!validationResult.success) {
+      throw new ApiError(
+        400,
+        'Validation failed',
+        validationResult.error.errors
+      );
+    }
+
+    const { token } = req.params;
+    const { password } = validationResult.data;
+    if (typeof token !== 'string') {
+      throw new ApiError(400, 'Invalid token');
+    }
+
+    const hashToken = crypto.createHash('sha256').update(token).digest('hex');
+
+    const user = await User.findOne({
+      resetPasswordToken: hashToken,
+      resetPasswordExpires: { $gt: new Date() },
+    });
+
+    if (!user) {
+      throw new ApiError(400, 'Invalid or expired toke');
+    }
+
+    // Hash and update password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+    user.resetPasswordToken = null;
+    user.resetPasswordExpires = null;
+
+    await user.save();
+
+    res.status(200).json(
+      new ApiResponse(200, 'password reset successfully', {
+        user: user.username,
+      })
+    );
   }
 );
--- server/src/validation/validationSchema.ts
+++ server/src/validation/validationSchema.ts
@@ -1,10 +1,17 @@
-import Zod from 'zod'
+import Zod from 'zod';
 
-export const resetPasswordSchema = Zod.object({
-    username: Zod.string()
-      .min(3, 'Username must be at least 3 characters')
-      .max(20, 'Username cannot exceed 20 characters').optional(),
-    email: Zod.string()
-      .email('Invalid email format')
-      .max(50, 'Email cannot exceed 50 characters'),
-  });
+export const requestResetPasswordSchema = Zod.object({
+  username: Zod.string()
+    .min(3, 'Username must be at least 3 characters')
+    .max(20, 'Username cannot exceed 20 characters')
+    .optional(),
+  email: Zod.string()
+    .email('Invalid email format')
+    .max(50, 'Email cannot exceed 50 characters'),
+});
+
+export const ResetPasswordSchema = Zod.object({
+  password: Zod.string()
+    .min(3, 'Username must be at least 3 characters')
+    .max(20, 'Username cannot exceed 20 characters')
+});
