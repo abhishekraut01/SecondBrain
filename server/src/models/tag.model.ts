aec1b513b71fb00c4a6da45fb371d2f3e275d667
Abhishek <Abhishekraut8697@gmail.com>
2025-02-06 19:55:19 +0530

    fix: Improve user model with password pre Hooks

--- server/src/models/tag.model.ts
+++ server/src/models/tag.model.ts
@@ -1,14 +1,25 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model, Document } from "mongoose";
 
-const tagSchema = new Schema(
+
+export interface ITag extends Document {
+  title: string;
+  readonly createdAt: Date;
+  readonly updatedAt: Date;
+}
+
+const tagSchema = new Schema<ITag>(
   {
     title: {
       type: String,
-      required: true,
+      required: [true, "Title is required"],
+      trim: true,
+      minlength: [3, "Title must be at least 3 characters long"],
+      maxlength: [50, "Title cannot exceed 50 characters"],
+      unique: true, 
     },
   },
   { timestamps: true }
 );
 
-const Tag = model('Tag', tagSchema);
-export default Tag;
+// Create and export the model
+export const Tag = model<ITag>("Tag", tagSchema);
--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,11 +1,9 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document } from 'mongoose';
+import bcrypt from 'bcryptjs';
 
-// Interface for User document
 interface IUser extends Document {
     username: string;
     password: string;
-    createdAt: Date;
-    updatedAt: Date;
 }
 
 const userSchema: Schema = new Schema({
@@ -18,10 +16,24 @@ const userSchema: Schema = new Schema({
         index: true
     },
     password: {
-        type: String,  // Fixed typo here
+        type: String,
         required: true,
     }
 }, { timestamps: true });
+
+// Hash the password before saving the user
+userSchema.pre<IUser>('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err as Error);
+    }
+});
 
 const User = mongoose.model<IUser>('User', userSchema);
 export default User;
