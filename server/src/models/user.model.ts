ed1130d2a33ab91c268210eadd2d70d67fe17583
Abhishek <Abhishekraut8697@gmail.com>
2025-02-06 20:12:46 +0530

    fix:Improved user model

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -2,38 +2,47 @@ import mongoose, { Schema, Document } from 'mongoose';
 import bcrypt from 'bcryptjs';
 
 interface IUser extends Document {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
 
-const userSchema: Schema = new Schema({
+const userSchema: Schema = new Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
     password: {
-        type: String,
-        required: true,
-    }
-}, { timestamps: true });
+      type: String,
+      required: true,
+      minlength: 8, // Example of password validation for minimum length
+    },
+  },
+  { timestamps: true }
+);
 
 // Hash the password before saving the user
 userSchema.pre<IUser>('save', async function (next) {
-    if (!this.isModified('password')) {
-        return next();
-    }
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (err) {
-        next(err as Error);
-    }
+  if (!this.isModified('password')) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err instanceof Error ? err : new Error(String(err)));
+  }
 });
+
+// Validate the password
+userSchema.methods.isPasswordValid = function (this: IUser, password: string) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = mongoose.model<IUser>('User', userSchema);
 export default User;
