ce1b47f1383fe095c53b856bdd421d51304fb6af
Abhishek <Abhishekraut8697@gmail.com>
2025-02-10 23:58:22 +0530

    feat: update saveLink controller to generate hash from link and check for existing links by hash

--- server/src/controllers/link.controller.ts
+++ server/src/controllers/link.controller.ts
@@ -3,34 +3,36 @@ import asyncHandler from '../utils/asyncHandler';
 import ApiError from '../utils/ApiError';
 import ApiResponse from '../utils/ApiResponse';
 import Link from '../models/link.model';
+import crypto from 'crypto'
 
 interface CustomRequest extends Request {
   user?: { _id: string };
 }
 
 export const saveLink = asyncHandler(async (req: CustomRequest, res: Response) => {
-  const { link, hash } = req.body;
-  const userId = req.user?._id;
+    const { link } = req.body;
+    const userId = req.user?._id;
+  
+    if (!userId) {
+      throw new ApiError(401, 'User is not authenticated');
+    }
+  
+    if (!link) {
+      throw new ApiError(400, 'Link is required');
+    }
 
-  if (!userId) {
-    throw new ApiError(401, 'User is not authenticated');
-  }
-
-  if (!link || !hash) {
-    throw new ApiError(400, 'Link and hash are required');
-  }
-
-  // Check if the link already exists for the user
-  const existingLink = await Link.findOne({ link, userId });
-  if (existingLink) {
-    throw new ApiError(400, 'This link is already saved');
-  }
-
-  const newLink = await Link.create({ link, hash, userId });
-
-  return res.status(201).json(new ApiResponse(201, 'Link saved successfully', newLink));
-});
-
+    const hash = crypto.createHash('sha256').update(link).digest('hex');
+  
+    const existingLink = await Link.findOne({ hash, userId });
+    if (existingLink) {
+      throw new ApiError(400, 'This link is already saved');
+    }
+  
+    
+    const newLink = await Link.create({ link, hash, userId });
+  
+    return res.status(201).json(new ApiResponse(201, 'Link saved successfully', newLink));
+  });
 
 export const getAllLinks = asyncHandler(async (req: CustomRequest, res: Response) => {
     const userId = req.user?._id;
@@ -65,5 +67,6 @@ export const getAllLinks = asyncHandler(async (req: CustomRequest, res: Response
   
     return res.status(200).json(new ApiResponse(200, 'Link fetched successfully', link));
   });
-  
+
+
  
