017e4f5a17e87f7439138e7a7d00f4f3408d26fe
Abhishek <Abhishekraut8697@gmail.com>
2025-02-12 17:38:18 +0530

    feat: implement getUserProfile controller for fetching user profile with authentication checks

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,12 +1,41 @@
 import { Request, Response } from 'express';
 import asyncHandler from '../utils/asyncHandler';
+import { ObjectId } from 'mongoose';
+import ApiError from '../utils/ApiError';
+import User from '../models/user.model';
+import ApiResponse from '../utils/ApiResponse';
+
+interface customRequest extends Request {
+  user?: {
+    _id: string | ObjectId;
+  };
+}
 
 export const getUserProfile = asyncHandler(
-  async (req: Request, res: Response) => {}
+  async (req: customRequest, res: Response) => {
+    const userId = req.user?._id;
+
+    if (!userId) {
+      throw new ApiError(401, 'User is not authenticated. Please log in.');
+    }
+
+    const user = await User.findById(userId)
+      .select("-password -resetPasswordToken -resetPasswordExpires -refreshToken")
+      .lean(); 
+
+    if (!user) {
+      throw new ApiError(404, 'User not found.');
+    }
+
+    res.status(200).json(new ApiResponse(200, 'User profile fetched successfully.', user));
+  }
 );
 
+
 export const updateUserProfile = asyncHandler(
-  async (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+
+  }
 );
 
 export const deleteUserProfile = asyncHandler(
